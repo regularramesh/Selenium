1c8aa5293cd1644f51be51f3ce1982f7041bb14b
Ramesh <Ramesh@DESKTOP-1O7GECF>
2021-06-05 22:56:38 +0530

    Selenium

---
+++ Selenium/src/Edit.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Edit {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "G:\\Selenium\\Drivers\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("http://www.leafground.com/pages/Edit.html");
+		
+		WebElement Email=driver.findElement(By.id("email"));
+		Email.sendKeys("regularramesh@gmail.com");
+		
+		WebElement Append=driver.findElement(By.xpath("//*[@id=\'contentblock\']/section/div[2]/div/div/input"));
+		Append.sendKeys("Text");
+		
+		WebElement Text=driver.findElement(By.name("username"));
+		String name=Text.getAttribute("value");
+		System.out.println(name);
+		
+		WebElement Clear=driver.findElement(By.xpath("//*[@id=\'contentblock\']/section/div[4]/div/div/input"));
+		Clear.clear();
+		
+		WebElement DisableBox=driver.findElement(By.xpath("//*[@id=\'contentblock\']/section/div[5]/div/div/input"));
+		boolean Disable=DisableBox.isEnabled();
+		System.out.println(Disable);
+	}
+
+}
